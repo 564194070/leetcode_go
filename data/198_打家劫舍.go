3534c4b2e26a485879e34ef7241bde61198413e4
江河湖海小浪浪 <564194070@qq.com>
2021-11-10 16:46:31 +0800

    198_打家劫舍增加解法

--- data/198_打家劫舍.go
+++ data/198_打家劫舍.go
@@ -26,3 +26,26 @@ func rob(nums []int) int {
 }
 
 //滚动数组 压缩状态
+func rob2(nums []int) int {
+
+	max := func(num1 int, num2 int) int {
+		if num1 > num2 {
+			return num1
+		}
+		return num2
+	}
+	lenNum := len(nums) - 1
+	if lenNum == 0 {
+		return nums[0]
+	}
+
+	arr := [3]int{}
+	arr[0], arr[1], arr[2] = nums[0], max(nums[0], nums[1]), max(nums[0], nums[1])
+
+	for i := 2; i < len(nums); i++ {
+		arr[2] = max(arr[0]+nums[i], arr[1])
+		arr[1], arr[0] = arr[2], arr[1]
+	}
+	return arr[2]
+
+}
